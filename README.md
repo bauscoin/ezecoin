b572bd39e64a18b45f8658ca9d1d43c5722f16ef
Eze <eze@Ezes-Mac-Pro.local>
2013-12-17 19:50:24 +1100

    changes

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-FooCoin (FOO)
+EzeCoin (EZE)
 ===========
 
-[![Build Status](https://travis-ci.org/RazorLove/foocoin.png?branch=master)](https://travis-ci.org/RazorLove/foocoin)
+[![Build Status](https://travis-ci.org/RazorLove/ezecoin.png?branch=master)](https://travis-ci.org/RazorLove/ezecoin)
 
 
 Scrypt Hashcash PoW Template
--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -45,13 +45,13 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("FooCoin");
-    case mBTC: return QString("milliFooCoin (1 / 1,000)");
-    case uBTC: return QString("microFooCoin (1 / 1,000,000)");
+    case BTC: return QString("EzeCoin");
+    case mBTC: return QString("milliEzeCoin (1 / 1,000)");
+    case uBTC: return QString("microEzeCoin (1 / 1,000,000)");
     default: return QString("???");
     }
 }
-//a single unit (.00000001) of FooCoin is called a "wander."
+//a single unit (.00000001) of EzeCoin is called a "wander."
 qint64 BitcoinUnits::factor(int unit)
 {
     switch(unit)
